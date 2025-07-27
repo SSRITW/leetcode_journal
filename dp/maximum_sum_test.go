ebe34fa4a67bdb6e8a7022becd9a056ce45a73ac
SSRITW <irostukai@gmail.com>
2025-07-27 17:01:31 +0900

    1186 删除一次得到子数组求最大和（中等） 26号开始挑战，今天才有头绪，很不擅长这种算法。

---
+++ dp/maximum_sum_test.go
@@ -0,0 +1,52 @@
+package dp
+
+import (
+	"testing"
+)
+
+// 1186 从数组中选出子数组（不能跳选，需要连续），选出之后可以选择要不要删除其中一个元素，求子数组删除后（如果有删）的最大和 （中等）
+// 子数组不能为空
+func maximumSum(arr []int) (ans int) {
+	arrLen := len(arr)
+
+	//left=以i结尾的最大元素和
+	left := make([]int, arrLen)
+	//right=以i开头的最大元素和
+	right := make([]int, arrLen)
+	left[0], right[arrLen-1], ans = arr[0], arr[arrLen-1], arr[0]
+
+	for i := 1; i < arrLen; i++ {
+		//如果之前的数已经小于0，重新开始累加
+		left[i] = max(left[i-1], 0) + arr[i]
+		right[arrLen-i-1] = max(right[arrLen-i], 0) + arr[arrLen-i-1]
+		//这里的ans是未删除情况下的最大值
+		ans = max(ans, left[i])
+	}
+
+	//通过循环left下标i-1和right下标i+1,找出删除1位的最大值
+	for i := 1; i < arrLen-1; i++ {
+		ans = max(ans, left[i-1]+right[i+1])
+	}
+
+	return
+}
+
+func TestMaximumSum(t *testing.T) {
+	testCases := []struct {
+		num      []int
+		expected int
+	}{
+		{num: []int{1, -2, 0, 3}, expected: 4},
+		{num: []int{1, -2, -2, 3}, expected: 3},
+		{num: []int{-1, -1, -1, -1}, expected: -1},
+		{num: []int{1, -4, -5, -2, 5, 0, -1, 2}, expected: 7},
+		{num: []int{8, -1, 6, -7, -4, 5, -4, 7, -6}, expected: 17},
+	}
+	t.Run("maximumSum", func(t *testing.T) {
+		for _, cases := range testCases {
+			if sum := maximumSum(cases.num); sum != cases.expected {
+				t.Errorf("num:=%v,expected: %d, got: %d", cases.num, cases.expected, sum)
+			}
+		}
+	})
+}
