24d9a22cc4725565454b1ff98e2c171f6e87d47c
SSRITW <irostukai@gmail.com>
2025-08-23 17:23:24 +0900

    138. 随机链表的复制(中等) 优化了第一个写法，不另外list存新节点，直接旧节点指向新节点； 增加递归实现方法。

--- linkedlist/copy_random_list_test.go
+++ linkedlist/copy_random_list_test.go
@@ -10,31 +10,50 @@ type RandomNode struct {
 
 func copyRandomList(head *RandomNode) *RandomNode {
 	p := head
-
 	//新的链表
 	newHead := &RandomNode{}
 	newP := newHead
-
-	//存储新链表的位置
-	nodeList := make([]*RandomNode, 0)
-	//旧链表的下标
-	pMap := map[*RandomNode]int{}
-	for i := 0; p != nil; i++ {
-		pMap[p] = i
+	//旧链表的节点指向新链表节点
+	pMap := map[*RandomNode]*RandomNode{}
+	for p != nil {
 		newP.Next = &RandomNode{Val: p.Val}
-		nodeList = append(nodeList, newP.Next)
+		pMap[p] = newP.Next
 		newP = newP.Next
 		p = p.Next
 	}
 	p = head
 	newP = newHead.Next
 	for p != nil {
-		//通过指针获得对应的下标，得到新节点
-		if _, ok := pMap[p.Random]; ok {
-			newP.Random = nodeList[pMap[p.Random]]
-		}
+		//通过指针得到新节点
+		newP.Random = pMap[p.Random]
 		newP = newP.Next
 		p = p.Next
 	}
 	return newHead.Next
 }
+
+// 使用递归方式实现，不变的是，还是用hash的key记录节点指针。
+
+var nodeMap map[*RandomNode]*RandomNode
+
+func copyRandomList2(head *RandomNode) *RandomNode {
+	nodeMap = map[*RandomNode]*RandomNode{}
+	return deepCopy(head)
+}
+
+func deepCopy(head *RandomNode) *RandomNode {
+	if head == nil {
+		return nil
+	}
+	//已经拷贝过
+	if _, ok := nodeMap[head]; ok {
+		return nodeMap[head]
+	}
+	newHead := &RandomNode{Val: head.Val}
+	nodeMap[head] = newHead
+	newHead.Next = deepCopy(head.Next)
+	newHead.Random = deepCopy(head.Random)
+	return newHead
+}
+
+//-----------------------------------------------------------------------
